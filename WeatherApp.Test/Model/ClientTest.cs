89fa7bdab1342de61bef56c205a12aa1ddd29d9d
1adangb <adan.no.adan@gmail.com>
2020-01-22 13:53:39 -0600

    Agragado ClientTest y corregido tipo de proyecto de test

---
+++ WeatherApp.Test/Model/ClientTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherApp.Model;
+
+namespace WeatherApp.Test
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void GetLocations()
+        {
+            var query = String.Empty;
+            var maxResults = -1;
+            var list = Client.GetLocations(query, maxResults);
+            Assert.AreEqual(list, null);
+            query = "Madrid";
+            maxResults = new Random().Next(0, 20);
+            list = Client.GetLocations(query, maxResults);
+            Assert.IsTrue(list != null && list.Count >= 0);
+        }
+
+        [TestMethod]
+        public void GetWeather()
+        {
+            var coords = new Coordinates
+            {
+                latitude = 0D,
+                longitude = 0D
+            };
+            var weather = Client.GetWeather(null);
+            Assert.IsNull(weather);
+            weather = Client.GetWeather(coords);
+            Assert.IsNotNull(weather);
+        }
+    }
+}
--- WeatherApp.Test/Properties/AssemblyInfo.cs
+++ WeatherApp.Test/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// La información general de un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
 [assembly: AssemblyTitle("WeatherApp.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
-// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
-// COM, establezca el atributo ComVisible en true en este tipo.
 [assembly: ComVisible(false)]
 
-// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("7796eecf-a52c-4909-ba82-9ee844cead98")]
+[assembly: Guid("a0012d26-c9e2-4d7b-8fb3-d8b1dd8b686d")]
 
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
-// utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
